1901d45017ac83bd8414229ca0a00ff4169eeabe
Alex Holanda <alex.holanda@outlook.com>
2021-09-22 11:53:44 -0300

    Configurado tema no styled-components

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from "styled-components";
+
+export const light: DefaultTheme = {
+  pageBackground: "#f3f8fa",
+  pageForeground: "#274060",
+  activeElementBackground: "#fff",
+  activeElementForeground: "#274060",
+  borderRadius: "10px",
+  inactiveElementBackground: "efefef",
+  inactiveElementForeground: "#959595",
+  inactiveElementOpacity: 0.5,
+  primaryBackground: "#09f",
+  primaryForeground: "#fff",
+};

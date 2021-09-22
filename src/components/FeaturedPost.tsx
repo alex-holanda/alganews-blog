905fcfd711d345244fba7499fedd003b2fae92ff
Alex Holanda <alex.holanda@outlook.com>
2021-09-22 18:51:06 -0300

    Criado o componente FeaturedPost

---
+++ src/components/FeaturedPost.tsx
@@ -0,0 +1,92 @@
+import { Post } from "alex-holanda-sdk";
+import styled from "styled-components";
+
+interface FeaturedPostProps {
+  postSummary: Post.Summary;
+}
+export default function FeaturedPost(props: FeaturedPostProps) {
+  return (
+    <Wrapper>
+      <Tags>
+        {props.postSummary.tags.map((tag) => (
+          <Tag key={tag}>{tag}</Tag>
+        ))}
+      </Tags>
+      <Editor>
+        <Avatar src={props.postSummary.imageUrls.small} />
+        <EditorDescription>
+          <EditorName>{props.postSummary.editor.name}</EditorName>
+          <PostDate>há 3 dias</PostDate>
+        </EditorDescription>
+      </Editor>
+
+      <Title>{props.postSummary.title}</Title>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  background-color: ${(props) => props.theme.primaryBackground};
+  color: ${(props) => props.theme.primaryForeground};
+  border-radius: ${(props) => props.theme.borderRadius};
+
+  padding: 32px;
+
+  gap: 24px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  width: 100%;
+  min-height: 256px;
+`;
+
+const Tags = styled.ul`
+  list-style: none;
+  display: flex;
+  gap: 8px;
+`;
+
+const Tag = styled.li`
+  background-color: ${(props) => props.theme.activeElementBackground};
+  color: ${(props) => props.theme.activeElementForeground};
+  border-radius: ${(props) => props.theme.borderRadius};
+
+  text-transform: lowercase;
+  padding: 4px 12px;
+  cursor: default;
+`;
+
+const Editor = styled.div`
+  display: flex;
+  gap: 16px;
+`;
+
+const EditorDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+const PostDate = styled.p`
+  font-size: 12px;
+`;
+
+const EditorName = styled.p`
+  font-size: 14px;
+  font-weight: 700;
+`;
+
+const Avatar = styled.img`
+  height: 40px;
+  width: 40px;
+  object-fit: cover;
+  border-radius: 20px;
+  box-shadow: 0 0 0 4px ${(props) => props.theme.primaryForeground};
+`;
+
+const Title = styled.h2`
+  font-size: 24px;
+  font-weight: 700;
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,70 @@
 import type { NextPage } from "next";
+import FeaturedPost from "../components/FeaturedPost";
+
+const fakePost = {
+  id: 45,
+  editor: {
+    id: 29,
+    name: "Daniel Bonifacio",
+    avatarUrls: {
+      default:
+        "https://storage.googleapis.com/alganews-files/posts/avatar-joao.jpeg",
+      small:
+        "https://storage.googleapis.com/alganews-files/posts/avatar-joao-small.jpeg",
+      medium:
+        "https://storage.googleapis.com/alganews-files/posts/avatar-joao-medium.jpeg",
+      large:
+        "https://storage.googleapis.com/alganews-files/posts/avatar-joao-large.jpeg",
+    },
+    createdAt: "2017-03-04T00:12:45Z",
+  },
+  slug: "como-fazer-x-coisas-com-react-js",
+  title: "Como fazer X coisas com React.js",
+  imageUrls: {
+    default:
+      "https://storage.googleapis.com/alganews-files/posts/avatar-joao.jpeg",
+    small:
+      "https://storage.googleapis.com/alganews-files/posts/avatar-joao-small.jpeg",
+    medium:
+      "https://storage.googleapis.com/alganews-files/posts/avatar-joao-medium.jpeg",
+    large:
+      "https://storage.googleapis.com/alganews-files/posts/avatar-joao-large.jpeg",
+  },
+  body: "Olá, hoje eu vou **mostrar** a como fazer X coisas com `react.js`\n",
+  tags: ["JavaScript", "react", "redux"],
+  createdAt: "2017-03-04T00:12:45-03:00",
+  earnings: {
+    pricePerWord: 0.06,
+    words: 2429,
+    totalAmount: 145.74,
+  },
+  published: true,
+  updatedAt: "2020-03-04T00:12:45-03:00Z",
+  updatedBy: {
+    id: 29,
+    name: "Daniel Bonifacio",
+    avatarUrls: {
+      default:
+        "https://storage.googleapis.com/alganews-files/posts/avatar-joao.jpeg",
+      small:
+        "https://storage.googleapis.com/alganews-files/posts/avatar-joao-small.jpeg",
+      medium:
+        "https://storage.googleapis.com/alganews-files/posts/avatar-joao-medium.jpeg",
+      large:
+        "https://storage.googleapis.com/alganews-files/posts/avatar-joao-large.jpeg",
+    },
+  },
+  canBePublished: true,
+  canBeUnpublished: true,
+  canBeDeleted: true,
+  canBeEdited: true,
+};
 
 const Home: NextPage = () => {
   return (
     <div>
       <h2>Olá mundo</h2>
+      <FeaturedPost postSummary={fakePost} />
     </div>
   );
 };

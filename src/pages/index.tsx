3a90a59724b46ba308b60fb149cee0d99ddaa7ea
Alex Holanda <alex.holanda@outlook.com>
2021-09-23 11:36:25 -0300

    Entendendo a diferença entre getServerSideProps e useEffect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,70 +1,31 @@
-import type { NextPage } from "next";
+import { Post, PostService } from "alex-holanda-sdk";
+
 import styled from "styled-components";
 import FeaturedPost from "../components/FeaturedPost";
 
-const fakePost = {
-  id: 45,
-  editor: {
-    id: 29,
-    name: "Daniel Bonifacio",
-    avatarUrls: {
-      default:
-        "https://storage.googleapis.com/alganews-files/posts/avatar-joao.jpeg",
-      small:
-        "https://storage.googleapis.com/alganews-files/posts/avatar-joao-small.jpeg",
-      medium:
-        "https://storage.googleapis.com/alganews-files/posts/avatar-joao-medium.jpeg",
-      large:
-        "https://storage.googleapis.com/alganews-files/posts/avatar-joao-large.jpeg",
-    },
-    createdAt: "2017-03-04T00:12:45Z",
-  },
-  slug: "como-fazer-x-coisas-com-react-js",
-  title: "Como fazer X coisas com React.js",
-  imageUrls: {
-    default: "/laptop.jpg",
-    small: "/laptop.jpg",
-    medium: "/laptop.jpg",
-    large: "/laptop.jpg",
-  },
-  body: "Olá, hoje eu vou **mostrar** a como fazer X coisas com `react.js`\n",
-  tags: ["JavaScript", "react", "redux"],
-  createdAt: "2017-03-04T00:12:45-03:00",
-  earnings: {
-    pricePerWord: 0.06,
-    words: 2429,
-    totalAmount: 145.74,
-  },
-  published: true,
-  updatedAt: "2020-03-04T00:12:45-03:00Z",
-  updatedBy: {
-    id: 29,
-    name: "Daniel Bonifacio",
-    avatarUrls: {
-      default:
-        "https://storage.googleapis.com/alganews-files/posts/avatar-joao.jpeg",
-      small:
-        "https://storage.googleapis.com/alganews-files/posts/avatar-joao-small.jpeg",
-      medium:
-        "https://storage.googleapis.com/alganews-files/posts/avatar-joao-medium.jpeg",
-      large:
-        "https://storage.googleapis.com/alganews-files/posts/avatar-joao-large.jpeg",
-    },
-  },
-  canBePublished: true,
-  canBeUnpublished: true,
-  canBeDeleted: true,
-  canBeEdited: true,
-};
+interface HomeProps {
+  posts: Post.Paginated;
+}
 
-const Home: NextPage = () => {
+function Home(props: HomeProps) {
   return (
     <Wrapper>
-      <FeaturedPost postSummary={fakePost} />
+      {props.posts?.content && (
+        <FeaturedPost postSummary={props.posts.content[0]} />
+      )}
     </Wrapper>
   );
-};
-
-export default Home;
+}
 
 const Wrapper = styled.div``;
+
+export async function getServerSideProps() {
+  const posts = await PostService.getAllPosts({ page: 0 });
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Home;
